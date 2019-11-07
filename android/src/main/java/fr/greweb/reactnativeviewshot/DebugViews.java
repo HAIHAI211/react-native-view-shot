6ce51ed3c59073017c03a169e2a5f176de7373cb
sunzheng <haihai211>
2019-11-07 17:28:39 +0800

    兼容rn0.59

--- android/src/main/java/fr/greweb/reactnativeviewshot/DebugViews.java
+++ android/src/main/java/fr/greweb/reactnativeviewshot/DebugViews.java
@@ -6,7 +6,7 @@ import android.content.res.Resources;
 import android.graphics.Matrix;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.core.util.Pair;
+import android.util.Pair;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
